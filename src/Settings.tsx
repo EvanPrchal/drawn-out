d83d955ad7c3f4d4caa9e4e5bd9b4c8c9b94823e
EvanPrchal <evanprchal@gmail.com>
2025-07-30 11:58:59 -0600

    settings padding update

--- src/Settings.tsx
+++ src/Settings.tsx
@@ -4,7 +4,7 @@ import Logo from "./Components/Logo";
 
 const Settings = () => {
   return (
-    <div className="flex flex-col justify-around items-center gap-25 h-full  bg-[url(/external/images/background.png)] bg-drawn-bg p-[6%]">
+    <div className="flex flex-col justify-around items-center gap-25 h-full  bg-[url(/external/images/background.png)] bg-drawn-bg p-[8%]">
       <h1 className="font-header text-body text-drawn-white rounded-full bg-drawn-black p-1 px-20 self-start ">Settings</h1>
       <section className="flex flex-col justify-around gap-60">
         <Dropdown className="  hover:cursor-pointer rounded-full p-1 px-20 bg-drawn-black border-7 border-drawn-black text-drawn-white text-nowrap flex flex-col justify-center items-center text-[40px] w-full`">
